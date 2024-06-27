40e6ab8e215227eabbd6b642a422c14fb36e1a8a
Nabneet Singh <nabneet.singh@autviz.in>
2024-06-27 12:04:21 +0530

    newchanges

--- authentication/models.py
+++ authentication/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Roles(models.IntegerChoices):
-    admin = 0,
-    student = 1       
-
+    ADMIN = 0, 
+    STUDENT = 1, 
 
 class CustomUser(AbstractUser):
     
-    role = models.IntegerField(choices=Roles,default=Roles.student)
+    role = models.IntegerField(choices=Roles.choices,default=Roles.STUDENT)
     created_by_user = models.ForeignKey('self', on_delete=models.SET_NULL, blank=True, null=True)
     
     def __str__(self):
